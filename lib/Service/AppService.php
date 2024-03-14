f7b80b0b3a027feb9bf96b6c8db3ce825308ae67
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-03-14 15:42:48 +0100

    extend app service

--- lib/Service/AppService.php
+++ lib/Service/AppService.php
@@ -4,10 +4,16 @@ namespace OCA\Overleaf\Service;
 
 use OCA\Overleaf\Settings\AppSettings;
 
+use OCP\IUserSession;
+
 class AppService {
+    private IUserSession $userSession;
+
     private AppSettings $settings;
 
-    public function __construct(AppSettings $settings) {
+    public function __construct(IUserSession $userSession, AppSettings $settings) {
+        $this->userSession = $userSession;
+
         $this->settings = $settings;
     }
 
@@ -24,6 +30,64 @@ class AppService {
             return "$host:$port";
         }
         return $host;
+    }
+
+
+    public function generateProjectsURL($userData): string {
+        $url = $this->settings->getAppURL();
+        if ($url == "") {
+            return "";
+        }
+
+        // Build the URL and redirect to it
+        $params = http_build_query([
+            "action" => "open-projects",
+            "data" => $userData,
+        ]);
+        return rtrim($url, "/") . "/regsvc?{$params}";
+    }
+
+    public function generateCreateAndLoginURL(): string {
+        $url = $this->settings->getAppURL();
+        if ($url == "") {
+            return "";
+        }
+
+        $user = $this->userSession->getUser();
+        if ($user == null) {
+            return "";
+        }
+
+        // Build the URL and redirect to it
+        $params = http_build_query([
+            "action" => "create-and-login",
+            "email" => $this->normalizeUserID($user->getUID()),
+        ]);
+        return rtrim($url, "/") . "/regsvc?{$params}";
+    }
+
+    public function generateDeleteUserURL($user): string {
+        $url = $this->settings->getAppURL();
+        if ($url == "") {
+            return "";
+        }
+
+        // Build the URL and redirect to it
+        $params = http_build_query([
+            "action" => "delete",
+            "email" => $this->normalizeUserID($user->getUID()),
+        ]);
+        return rtrim($url, "/") . "/regsvc?{$params}";
+    }
+
+    public function generatePassword(int $length = 64) {
+        $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+        $randomString = "";
+        for ($i = 0; $i < $length; $i++) {
+            $index = rand(0, strlen($characters) - 1);
+            $randomString .= $characters[$index];
+        }
+        return $randomString;
     }
 
     public function normalizeUserID(string $uid): string {
