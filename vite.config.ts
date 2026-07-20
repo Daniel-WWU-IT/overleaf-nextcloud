237555d4524f92c9023b098a8806e84a3d25022b
Daniel Müller <daniel.mueller@uni-muenster.de>
2026-07-20 11:00:49 +0200

    remove obsolete vite entry

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { join, resolve } from "path";
 
 export default createAppConfig(
 	{
-		files_integration: resolve(join("src", "files/integration.js")),
 		launcher_app: resolve(join("src", "launcher/app.js")),
 		launcher_launcher: resolve(join("src", "launcher/launcher.js")),
 		settings_appsettings: resolve(join("src", "settings/appsettings.js")),
