983fe44505523921983be064487beef7f3566112
Daniel Müller <daniel.mueller@uni-muenster.de>
2026-08-03 14:41:44 +0200

    add popup notice

--- js/launcher/launcher.js
+++ js/launcher/launcher.js
@@ -1,9 +1,25 @@
 'use strict';
 
 $(document).ready(() => {
+    const storageKey = "overleaf-v3-notice-hidden";
+    const sessionKey = "overleaf-v3-notice-shown";
+
     $("#app-frame").on("load", () => {
         $("#app-loading").hide();
         $("#app-frame").show();
         $('#app-frame').css('background-color', '#fff');
+
+        if (localStorage.getItem(storageKey) !== "true" && sessionStorage.getItem(sessionKey) !== "true") {
+            $("#overlay").css('display', 'flex');
+            sessionStorage.setItem(sessionKey, "true");
+        }
+    });
+
+    $("#closePopup").click(() => {
+        if ($("#dontShowAgain").is(":checked")) {
+            localStorage.setItem(storageKey, "true");
+        }
+
+        $("#overlay").hide();
     });
 });
