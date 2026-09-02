c85d0c5386c054331c3ee07b4a804cbbf44ecef9
Daniel Müller <daniel.mueller@uni-muenster.de>
2026-09-02 09:57:14 +0200

    add jquery

--- templates/launcher/debug.php
+++ templates/launcher/debug.php
@@ -4,6 +4,8 @@ use OCP\Util;
 
 use OCA\OverleafV6\AppInfo\Application;
 
+Util::addScript(Application::APP_ID, Application::APP_ID . "-jquery");
+
 Util::addStyle(Application::APP_ID, "main");
 Util::addStyle(Application::APP_ID, "launcher/launcher");
 ?>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { join, resolve } from "path";
 
 export default createAppConfig(
 	{
+		jquery: resolve(join("src", "lib/jquery.js")),
 		launcher_app: resolve(join("src", "launcher/app.js")),
 		launcher_launcher: resolve(join("src", "launcher/launcher.js")),
 		settings_appsettings: resolve(join("src", "settings/appsettings.js")),
